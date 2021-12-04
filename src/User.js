6d266f632bda6008b87d7ee92902605c89bf55ce
Nerf51 <ngyesuntraidor@gmail.com>
2021-12-04 15:23:12 +0100

    dewwfed

--- src/User.js
+++ src/User.js
@@ -7,7 +7,11 @@ const User = () => {
     const [animes, setAnimes] = useState();
     useEffect(() => {
         fetch(`https://ticket-counter.herokuapp.com/tickets/${id}`).then(res => res.json()).then(response => {
-            setAnimes(response.map(x => <picture className="container" key={x.date}><Avatar className="avatar" alt={x.author} src={x.avatar}/><p className="message">{x.author}: {x.message}</p><br></br></picture>))
+            setAnimes(response.map(x => <picture className="container" key={x.date}>
+                <Avatar className="avatar" alt={x.author} src={x.avatar}/>
+                <p className="message">{x.author}: {x.message}</p>
+                <br></br>
+                </picture>))
         })
     }, [])
     return (
