fcd4613467ba97d0ef2e929fab040370daff1e95
Nerf51 <ngyesuntraidor@gmail.com>
2021-12-04 16:40:17 +0100

    dewwfefed

--- src/User.js
+++ src/User.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
+import CircularProgress from '@mui/material/CircularProgress';
 const User = () => {
     const { id } = useParams();
     const [animes, setAnimes] = useState();
+    const [loaded, setLoaded] = useState("loaded");
     useEffect(() => {
         fetch(`https://ticket-counter.herokuapp.com/tickets/${id}`).then(res => res.json()).then(response => {
             setAnimes(response.map(x => <picture className="container" key={x.date}>
@@ -12,10 +14,13 @@ const User = () => {
                 <p className="message">{x.author}: {x.message}</p>
                 <br></br>
                 </picture>))
+                
+            setLoaded("notLoaded")
         })
     }, [])
     return (
         <div className="main">
+            <div className={loaded}><CircularProgress className="progress" /></div>
             {animes}
         </div>
     );
