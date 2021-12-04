cb97d7fcc7bc7433b07c9419234f7b168f0ad364
Nerf51 <ngyesuntraidor@gmail.com>
2021-12-04 23:11:12 +0100

    dewwferfefef7d

--- src/User.js
+++ src/User.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import CircularProgress from '@mui/material/CircularProgress';
+import moment from "moment";
 const User = () => {
     const { id } = useParams();
     const [animes, setAnimes] = useState();
@@ -11,7 +12,10 @@ const User = () => {
         fetch(`https://ticket-counter.herokuapp.com/tickets/${id}`).then(res => res.json()).then(response => {
             setAnimes(response.map(x => <picture className="container" key={x.date}>
                 <Avatar className="avatar" alt={x.author} src={x.avatar}/>
-                <p className="message">{x.author}: {x.message}</p>
+                <div>
+                    <p className="message-top"><b id="author">{x.author}</b> <small>{moment(x.timestamp).calendar()}</small></p>
+                    <p className="message-bottom">{x.message}</p>
+                </div>
                 <br></br>
                 </picture>))
                 
