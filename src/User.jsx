99ec4653ae710defd2515567fa9da27eebd8f063
Nerf51 <ngyesuntraidor@gmail.com>
2021-12-05 17:01:03 +0100

    dewwfsdferfdvssdfed

--- src/User.jsx
+++ src/User.jsx
@@ -11,9 +11,13 @@ const User = () => {
     const [loaded, setLoaded] = useState("loaded");
     const [active, setActive] = useState(false);
     const [image, setImage] = useState("");
+    const [error, setError] = useState("notLoaded");
     useEffect(() => {
         fetch(`https://ticket-counter.herokuapp.com/tickets/${id}`).then(res => res.json()).then(response => {
-            console.log(response)
+            if(response.status){
+                setLoaded("notLoaded")
+                return setError("container-error")
+            }
             setAnimes(response.map(x => <picture className="container" key={x.date}>
                 <Avatar className="avatar" alt={x.author} src={x.avatar} onClick={el => {
                     setImage(el.target.currentSrc)
@@ -29,18 +33,25 @@ const User = () => {
             setLoaded("notLoaded")
         })
     }, [])
-    return (
-        <div className="main">
-            <Backdrop
-                sx={{ color: '#fff', zIndex: 50}}
-                open={active}
-                onClick={() => setActive(false)}
-            >
-                <img src={image} alt="imagen" id="imagen"></img>
-            </Backdrop>
-            <div className={loaded}><CircularProgress className="progress" /></div>
-            {animes}
-        </div>
-    );
-}
+        return (
+            <div className="main">
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: 50}}
+                    open={active}
+                    onClick={() => setActive(false)}
+                >
+                    <img src={image} alt="imagen" id="imagen"></img>
+                </Backdrop>
+                <div className={loaded}><CircularProgress className="progress" /></div>
+                <div className={error}>
+                    <div className="container-error">
+                        <h2 className="error">ERROR</h2>
+                        <h1 className="error-four">404</h1>
+                        <h2 className="error" id="last">PAGE NOT FOUND</h2>
+                    </div>
+                </div>
+                {animes}
+            </div>
+        );
+    }
 export default User;
